8e395ce79dbc519408c3b417e89667c93e1f339c
Nurus Shakib Rhythm <84782668+nurusshakib88@users.noreply.github.com>
2021-07-25 18:20:15 +0600

    Add files via upload
    Bootstrap components practice (2nd half)

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// tooltip js
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+//popover js
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+//popover js (Dismissible)
+var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+  trigger: 'focus'
+})
+
